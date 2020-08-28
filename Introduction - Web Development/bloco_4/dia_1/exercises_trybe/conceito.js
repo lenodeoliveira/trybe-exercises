f1631a4befb3c8dda597c6afc7c64714a381bee3
Lenodeoliveira <lenodeoliveira@gmail.com>
2020-08-28 10:34:39 -0300

    Adicionando exercicio de conceitos

--- Introduction - Web Development/bloco_4/dia_1/exercises_trybe/conceito.js
+++ Introduction - Web Development/bloco_4/dia_1/exercises_trybe/conceito.js
@@ -1,31 +1,26 @@
-let conceito = 91;
+let conceito = 1;
 
-if (conceito >= 50) {
-    
-    switch (conceito) {
-        case 90:
-            console.log("A");
-            break;
-        
-        case 80: 
-            console.log("B");
-            break;
-        
-        case 70:    
-            console.log("C");
-            break;
-        
-        case 60:    
-            console.log("D");
-            break;
-        
-        case 50:
-            console.log("E");
-            break;
-            
-        default:
-            console.log("F");
-            break;
-    }
+if (conceito >= 90) {
 
+    console.log('A');
+}
+else if (conceito >= 80 && conceito < 90)
+{
+    console.log('B');
+}
+else if (conceito >= 70 && conceito < 80)
+{
+    console.log('C');
+}
+else if (conceito >= 60 && conceito < 70)
+{
+    console.log('D');
+}
+else if(conceito >= 50 && conceito < 60)
+{
+    console.log('E');
+}
+else
+{
+    console.log('F');
 }
