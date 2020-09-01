37873ea15ee90e264b6ba97ef50dd9c2107e6002
Lenodeoliveira <lenodeoliveira@gmail.com>
2020-09-01 18:43:26 -0300

    Adicionando exercícios de funções

---
+++ Introduction - Web Development/bloco_4/dia_4/funcoes/f-01.js
@@ -0,0 +1,18 @@
+
+function palindromo(palavra){
+
+ let reverse = palavra.split("").reverse().join("");
+
+    if (reverse === palavra) {
+      
+        return true;
+  
+    } else {
+    
+        return false;
+  
+    }
+
+}
+
+console.log(palindromo('arara'));
