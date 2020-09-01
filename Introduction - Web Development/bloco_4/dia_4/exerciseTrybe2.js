770841474b4c6e365004af90ce348ffc66b2d650
Lenodeoliveira <lenodeoliveira@gmail.com>
2020-09-01 18:07:39 -0300

    exercícioTrybe 5 sobre forin

--- Introduction - Web Development/bloco_4/dia_4/exerciseTrybe2.js
+++ Introduction - Web Development/bloco_4/dia_4/exerciseTrybe2.js
@@ -2,7 +2,9 @@ let info = {
     personagem: "Margarida",
     origem: "Pato Donald",
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-    recorrente: "Sim"
+    
   };
+
+  info.recorrente = "Sim";
 
   console.log(info);
