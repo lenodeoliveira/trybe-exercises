5cd610defc255a9d67b4da3f533115699e79bccd
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-04-17 18:27:46 -0300

    Adicionando exercícios aula 1 bloco 28

---
+++ Back-end/Bloco_28/aula_1/node-js-jwt-base-project/controllers/posts.js
@@ -0,0 +1,24 @@
+const mockPosts = [
+  {
+    title: 'título fake',
+    content: 'conteúdo conteúdo conteúdo conteúdo conteúdo',
+  },
+  {
+    title: 'título fake',
+    content: 'conteúdo conteúdo conteúdo conteúdo conteúdo',
+  },
+  {
+    title: 'título fake',
+    content: 'conteúdo conteúdo conteúdo conteúdo conteúdo',
+  },
+  {
+    title: 'título fake',
+    content: 'conteúdo conteúdo conteúdo conteúdo conteúdo',
+  },
+];
+
+module.exports = (req, res, next) => {
+  console.log(req.user);
+
+  res.status(200).json({ mockPosts });
+};
